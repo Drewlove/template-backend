be5c9060b0a794745e3d8a8a50628bbff388d934
Drew Love <davidandrewlove84@gmail.com>
2021-01-06 16:39:43 -0500

    Update service.js

--- src/boierplate-endpoints/service.js
+++ src/boierplate-endpoints/service.js
@@ -1,38 +1,32 @@
-//change tableName, getById include actual column name of row id, example 'student_id'
-const tableName = 'table_name'
-
+//REWRITE, make note of 'orderRow' if you want rows returned alphabetically
+const table = {
+  name: "challenge",
+  orderRow: "challenge_name",
+};
 const service = {
-    getAllRows(knex){
-        return knex
-        .select('*')
-        .from(tableName)
-    }, 
-    getById(knex, id){
-        return knex
-        .from(tableName)
-        .select('*')
-        .where('row_id', id)
-        .first()
-    }, 
-    insertRow(knex, newRow){
-        return knex
-        .insert(newRow)
-        .into(tableName)
-        .returning('*')
-        .then(rows => {
-            return rows[0]
-        })
-    }, 
-    updateRow(knex, id, newFields){
-        return knex(tableName)
-        .where({id})
-        .update(newFields)
-    }, 
-    deleteRow(knex, id){
-        return knex(tableName)
-        .where({id})
-        .delete()
-    }
-}
-
-module.exports = service
+  getAllRows(knex) {
+    return knex.select("*").from(table.name).orderBy(table.orderRow, "ASC");
+  },
+  getById(knex, row_id) {
+    return knex
+      .from(table.name)
+      .select("*")
+      .where(`${table.name}_id`, row_id)
+      .first();
+  },
+  insertRow(knex, newRow) {
+    return knex
+      .insert(newRow)
+      .into(table.name)
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  updateRow(knex, row_id, newFields) {
+    return knex(table.name).where(`${table.name}_id`, row_id).update(newFields);
+  },
+  deleteRow(knex, row_id) {
+    return knex(table.name).where(`${table.name}_id`, row_id).delete();
+  },
+};
